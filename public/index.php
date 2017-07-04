d27a1cfd3d96db73971f9f42ecbe67b0b9971f88
Alexander Isik <isik.alexander@yandex.ru>
2017-07-04 14:49:49 +0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,1 +1,15 @@
+<?php
 
+/**
+ * The Raice Framework.
+ *
+ * @link 
+ * @copyright Copyright (c) 2017 The Raice Framework
+ * @license 
+ */
+
+require '../env.php';
+require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../autoload.php';
+
+$app = new \Raice\Bootstrap( ROOT_PATH );
