9c0424c8f8d172096b9bb7f219347e8b3d7b00d7
Alexander Isik <isik.alexander@yandex.ru>
2017-07-04 04:38:34 +0300

    Update autoload.php

--- autoload.php
+++ autoload.php
@@ -1,1 +1,15 @@
+<?php
 
+function __autoload ( $class ) {
+    
+    if ( file_exists( $class . '.php' ) ) {
+          
+          require_once $class . '.php';
+         
+    } else {
+          
+          throw new \Exception('The ' . $class . ' class doesn\'t exist!');
+          
+    }
+      
+} 
