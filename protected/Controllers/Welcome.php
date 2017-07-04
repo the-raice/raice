9926b0e652f8557fab0384a6c4873c462349ea66
Alexander Isik <isik.alexander@yandex.ru>
2017-07-04 08:38:57 +0300

    Update Welcome.php

--- protected/Controllers/Welcome.php
+++ protected/Controllers/Welcome.php
@@ -3,8 +3,15 @@
 namespace Controllers;
 
 use Raice\Controller as Controller;
-
+    
 class Welcome extends Controller
 {
+    
+    public function __construct ()
+    {
   
+        parent::__construct( $this->getName() );
+        
+    }
+    
 }
