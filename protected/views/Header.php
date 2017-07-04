c8877dfb297223d3780712c8eeebdde8346e14c8
Alexander Isik <isik.alexander@yandex.ru>
2017-07-04 16:01:51 +0300

    Update Header.php

--- protected/views/Header.php
+++ protected/views/Header.php
@@ -3,10 +3,7 @@
 <head>
     <title>The Raice Framework</title>
     <meta charset="utf-8">
-    <link rel="stylesheet" type="text/css" href="/public/assets/css/default.css">
+    <link rel="stylesheet" type="text/css" href="/assets/css/default.css">
 </head>
 <body>
-    <header>
-        <h1 class="header__title">The Raice Framework</h1>
-    </header>
     <div class="content">
