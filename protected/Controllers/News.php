08343ee2c78aa063e826ee42b8888c4fadbdbd66
Alexander Isik <isik.alexander@yandex.ru>
2017-12-09 20:58:59 +1000

    Update News.php

--- protected/Controllers/News.php
+++ protected/Controllers/News.php
@@ -3,15 +3,38 @@
 namespace Controllers;
 
 use Raice\Controller as Controller;
+use Models\Article as Article;
     
 class News extends Controller
 {
-    
+	
+	
+	public $article;
+	
     public function __construct ()
     {
   
         parent::__construct( $this->getName() );
         
     }
+	
+	public function __call ( $id, $arguments = false )
+	{
+		
+		$id = (int) $id;
+		
+		if ( is_integer( $id ) && $id !== 0 ) {
+			
+			$this->article = Article::getOneById( $id );
+			
+		} else {
+			
+			$url = strtolower( $id );
+
+			$this->article = Article::getOneByField( $url, 'url' );
+			
+		}
+		
+	}
     
 }
