71bff92a6c4cdf3b9fe44cd732d358580f0e888c
Alexander Isik <isik.alexander@yandex.ru>
2017-12-10 14:04:44 +1000

    Create Article.php

---
+++ protected/views/Article/Article.php
@@ -0,0 +1,12 @@
+        <h1 class="content__title"><?=$data['title'] ?></h1>
+        <article class="content__article">
+            <p class="atricle__text"><?=$data['content'] ?></p>
+        </article>
+        <div class="content_metabar">
+            <ul class="metabar__list">
+                <li>
+                    <img src="<?=$data['author-photo'] ?>">
+                    <h3><?=$data['author'] ?></h3>
+                </li>
+            </ul>
+        </div>
