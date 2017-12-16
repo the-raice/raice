a3804f10b20b5541bc27dcee574bea87e9c95f2d
Alexander Isik <isik.alexander@yandex.ru>
2017-12-16 22:21:43 +1000

    Update Home.php

--- protected/views/Home/Home.php
+++ protected/views/Home/Home.php
@@ -1,2 +1,4 @@
-        <h1 class="content__title">The Raice Framework</h1>
-        <h3 class="content__description">v. <?=\Raice\VERSION ?></h1>
+        <div class="content__home">
+            <h1 class="content__title">The Raice Framework</h1>
+            <span class="content__description">v. <?=\Raice\VERSION ?></span>
+        </div>
