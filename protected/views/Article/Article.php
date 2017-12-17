7e8b09d4455a5756e34fd5e6d934f45093d7d4ff
Alexander Isik <isik.alexander@yandex.ru>
2017-12-17 13:35:59 +1000

    Update Article.php

--- protected/views/Article/Article.php
+++ protected/views/Article/Article.php
@@ -1,12 +1,26 @@
-        <h1 class="content__title"><?=$data['title'] ?></h1>
-        <article class="content__article">
-            <p class="atricle__text"><?=$data['content'] ?></p>
-        </article>
-        <div class="content_metabar">
-            <ul class="metabar__list">
-                <li>
-                    <img src="<?=$data['author-photo'] ?>">
-                    <h3><?=$data['author'] ?></h3>
-                </li>
-            </ul>
+        <div class="content__news">
+            <h1 class="news__title"><?=$data['title'] ?></h1>
+            <article class="content__article">
+                <br>
+                <h3 class="article__text"><?=$data['content'] ?></h3>
+            </article>
+            <div class="content_metabar">
+                <ul class="metabar__list">
+                    <li>
+                       <h3><?=$data['date'] ?></h3>
+                    </li>
+                    <li>
+                        <img class="author__image" src="<?=Models\User::getUserInfo( $data['author_id'] )[0]['image'] ?>">
+                        <a class="metabar__option" href="/user/<?=Models\User::getUserInfo( $data['author_id'] )[0]['username'] ?>"><h3><?=Models\User::getUserInfo( $data['author_id'] )[0]['username'] ?></h3>
+                    </li>
+<?php if ( Models\User::isEditor( $_SESSION['authorized'], $data['author_id'] ) ): ?>
+                    <li>
+                        <a class="metabar__option" href="/article/<?=$data['url'] ?>/edit"><h3>Edit</h3></a>
+                    </li>
+                    <li>
+                        <a class="metabar__option" href="/article/<?=$data['url'] ?>/delete"><h3>Delete</h3></a>
+                    </li>
+<?php endif; ?>                   
+                </ul>
+            </div>
         </div>
