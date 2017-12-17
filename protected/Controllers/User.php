773837c0182be5f48bafca8a66c3ae8162b13a6a
Alexander Isik <isik.alexander@yandex.ru>
2017-12-17 13:37:44 +1000

    Create User.php

---
+++ protected/Controllers/User.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Controllers;
+
+use Raice\Controller as Controller;
+use Models\User as User;
+    
+class User
+    extends Controller
+{
+    
+    
+    public $user;
+    
+    public function __call ( $url, $arguments = false )
+    {
+        
+        session_start();
+        
+        $url = strtolower( $url);
+
+        $this->user = \Models\User::getOneByField( $url, 'username' )[0];
+        
+        parent::getView( $this->getName(), $this->user );
+
+    }
+    
+}
